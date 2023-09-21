20f03a43151b8e32c60f9ad36d402c4a9b83d2a8
Makx3 <gatoconb42@gmail.com>
2023-09-20 22:39:27 -0300

    merchh

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,8 @@ import java.util.Scanner;
 // a
 import java.util.InputMismatchException;
 import java.util.Scanner;
-
-
-
-        ejecutarCalculadora();
-    }
-
-    public static void ejecutarCalculadora() {
-        Scanner scanner = new Scanner(System.in);
+public static void Main() {
+    Scanner scanner = new Scanner(System.in);
         int opcion;
 
         do {
