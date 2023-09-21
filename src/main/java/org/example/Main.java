fab1e7820ac0e642bb6093ab99a0067ebd84048b
Makx3 <142506889+Makx3@users.noreply.github.com>
2023-09-20 23:00:01 -0300

    Update and rename CalculadoraGeometrica.java to Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class CalculadoraGeometrica {
+public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
